6dd386f42df4f7031d3216e3c44f022519e242ae
Muhammad Fahad <siddiquiboy360@outlook.com>
2020-12-19 18:44:07 +0500

    Add project files.

---
+++ propertiesandIndexesApp/Calculate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace propertiesandIndexesApp
+{
+    class Calculate
+    {
+        int AccountBalance = 25600;
+
+        //Readwrite Property
+        public int balance
+        {
+            get { return AccountBalance; } //getter Method [For Reading]
+            set { AccountBalance = value; } //setter Method [For Writing]
+        }
+
+        public int readBalance
+        {
+            get { return AccountBalance; }
+        }
+
+        public int writeBalance
+        {
+            set { AccountBalance = value; }
+        }
+
+        //Auto Implemented property
+        public string name { get; set; }
+
+        //Auto Implemented Property Read Only
+        public string readName { get; private set; }
+
+        //Auto Implemented Property Write Only
+        public string writeName { private get; set; }
+    }
+}
---
+++ propertiesandIndexesApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace propertiesandIndexesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculate c = new Calculate();
+
+            //Console.WriteLine(c.readBalance);
+            //c.writeBalance = 50000;
+
+            //Console.WriteLine(c.readBalance);
+
+            //Console.ReadKey();
+
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student { enrolmentId = 1, name = "Fahad" });
+            students.Add(new Student { enrolmentId = 2, name = "Huzaifa" });
+            students.Add(new Student { enrolmentId = 3, name = "Javed" });
+            students.Add(new Student { enrolmentId = 4, name = "Hamza" });
+            students.Add(new Student { enrolmentId = 5, name = "Hyder" });
+            students.Add(new Student { enrolmentId = 6, name = "Tufail" });
+            students.Add(new Student { enrolmentId = 7, name = "Zeeshan" });
+            students.Add(new Student { enrolmentId = 8, name = "Mohsin" });
+
+            foreach (Student student in students)
+            {
+                Console.WriteLine("Enrollment Ids: {0}",student.enrolmentId);
+                Console.WriteLine("Name: {0}\n",student.name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
